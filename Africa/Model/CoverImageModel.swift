7780303437229d42d1bad348eb24869326ec6d13
hustin-aquino <justinmosesbitancor@gmail.com>
2024-10-11 18:19:47 +0800

    Displaying cover image with actual data from json file

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by justin.bitancor on 10/11/24.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
---
+++ Africa/View/CoverImageView.swift
@@ -0,0 +1,39 @@
+//
+//  CoverImageView.swift
+//  Africa
+//
+//  Created by justin.bitancor on 10/11/24.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    // MARK: - PROPERTIES
+    
+    let coverImages: [CoverImage] = Bundle.main.decode("covers.json")
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        TabView {
+            
+            ForEach(coverImages) { item in
+                Image(item.name)
+                    .resizable()
+                    .scaledToFill()
+            } //: LOOP
+        }//: TAB
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
+
+
+// MARK: - PREVIEW
+struct CoverImageView_Preview: PreviewProvider {
+    
+    static var previews: some View {
+        CoverImageView()
+            .previewLayout(.fixed(width: 400, height: 300))
+        
+    }
+}
