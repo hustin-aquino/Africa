f75e1970e3cc40df6140c57ca87ed15d6c45d1bc
hustin-aquino <justinmosesbitancor@gmail.com>
2024-10-11 17:31:46 +0800

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by justin.bitancor on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
