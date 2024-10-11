4e4e12767770fd2bfea5aa7b0ef04aac086a6b07
hustin-aquino <justinmosesbitancor@gmail.com>
2024-10-12 02:04:31 +0800

    Play selected video from VideoTab

---
+++ Africa/Helper/VideoPlayerHelper.swift
@@ -0,0 +1,20 @@
+//
+//  VideoPlayerHelper.swift
+//  Africa
+//
+//  Created by justin.bitancor on 10/12/24.
+//
+
+import Foundation
+import AVKit
+
+var videoPlayer: AVPlayer?
+
+func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
+    if let url = Bundle.main.url(forResource: fileName, withExtension: fileFormat) {
+        videoPlayer = AVPlayer(url: url)
+        videoPlayer?.play()
+    }
+    
+    return videoPlayer!
+}
